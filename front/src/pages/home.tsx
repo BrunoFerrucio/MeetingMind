986fe6bc9d7bbf1fe9130de57b46a3c11195e7b5
Bruno Ferrucio <brunofas1@gmail.com>
2025-07-15 13:43:03 -0300

    Add DaisyUI integration

--- front/src/pages/home.tsx
+++ front/src/pages/home.tsx
@@ -2,7 +2,13 @@ export function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Welcome to the home page!</p>
+      <p>
+        Welcome to the home page!
+        <div className="inline-grid *:[grid-area:1/1]">
+          <div className="status status-error animate-ping"></div>
+          <div className="status status-error"></div>
+        </div> Server is down
+      </p>
     </div>
   );
 }
