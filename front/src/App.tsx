f6d9184ee0ed2d33beb3fcbf5aaec415b7cec596
Bruno Ferrucio <brunofas1@gmail.com>
2025-07-15 14:25:07 -0300

    Update routing in App component to include Dashboard and Waitlist pages

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,12 +3,15 @@ import './App.css'
 import LandingPage from './pages/home'
 import DashboardIndex from './pages/dashboard'
 import WaitlistPage from './pages/waitlist-page'
+import TermsPage from './pages/terms'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} index />
+        <Route path="/dashboard" element={<DashboardIndex />} />
+        <Route path="/waitlist" element={<WaitlistPage />} />
         <Route path="/terms-of-use" element={<TermsPage />} />
       </Routes>
     </BrowserRouter>
