85b8b821b528de00399623f361edeafb7fa6e7fe
Bruno Ferrucio <brunofas1@gmail.com>
2025-07-15 13:58:27 -0300

    Add DashboardIndex component and update routing in App

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './pages/home'
+import DashboardIndex from './pages/dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} index />
+        <Route path="/dashboard" element={<DashboardIndex />} index />
       </Routes>
     </BrowserRouter>
   )
