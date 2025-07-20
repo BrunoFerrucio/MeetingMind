494d573fc20c93c982356135f6656178f98f8285
Bruno Ferrucio <brunofas1@gmail.com>
2025-07-20 00:34:42 -0300

    Refactor DashboardIndex layout: add overflow-y-auto to improve scrolling behavior

--- front/src/pages/dashboard/index.tsx
+++ front/src/pages/dashboard/index.tsx
@@ -5,7 +5,7 @@ export default function DashboardIndex() {
   return (
     <div className="h-screen w-screen flex flex-row">
       <Sidebar />
-      <div className="p-4">
+      <div className="p-4 w-full overflow-y-auto">
         <DashboardHome />
       </div>
     </div>
